e89c2a4f832fdf7f46f60c8e41635835c5a7f614
40618 <40618@longroad.ac.uk>
2015-01-06 13:04:12 +0000

    KD List

---
+++ KD_list.py
@@ -0,0 +1,24 @@
+
+players = [
+    ["k1llmAchine",51,49],
+    ["bob2247",5,99],
+    ["hAxOr12",72,30]
+]
+
+
+for count in range(3):
+    print("{0}".format(players[0][count]))
+
+for count in range(3):
+    print("{0}".format(players[1][count]))
+
+for count in range(3):
+    print("{0}".format(players[2][count]))
+
+
+
+
+
+
+
+
